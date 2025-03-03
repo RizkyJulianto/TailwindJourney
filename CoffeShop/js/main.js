f509a529e85a45a1d8bfd88341a4a1939c9b5e37
Juliantomaga <juliantorizky01@gmail.com>
2025-03-03 10:23:28 +0700

    make responsive header section in tablet view

--- CoffeShop/js/main.js
+++ CoffeShop/js/main.js
@@ -3,8 +3,26 @@ window.addEventListener('scroll' ,function() {
     const ScrollY = window.pageYOffset;
     const navContainer = document.querySelector('.navigation-container');
     if(ScrollY > 5) {
-       navContainer.classList.add('nav-active');
+       navContainer.classList.add('nav-scroll');
     } else {
-        navContainer.classList.remove('nav-active');
+        navContainer.classList.remove('nav-scroll');
     }
 })
+
+
+// Responsive Navbar
+const navigation = document.getElementById('navigation');
+const hamburger = document.querySelector('#hamburger');
+
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navigation.classList.toggle('nav-active');
+});
+
+navLinks.forEach(anchor => {
+    anchor.addEventListener('click', function() {
+        navigation.classList.remove('nav-active');
+        hamburger.classList.remove('hamburger-active');
+    });
+});
